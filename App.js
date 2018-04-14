cfb876338c1daf8547a3bb284c79372a627b1f1e
sherriff <sherriff.agboola@ikooba.com>
2018-04-14 21:18:34 +0100

    >> commit

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import { ApolloProvider } from 'react-apollo';
-
 import PixNavigation from './PixNavigation';
 import {client } from './src/graph/client';
 
